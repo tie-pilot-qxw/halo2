6dfb62c8678a45464fce52dddb79e0b698b9aefb
QiangXinwei <qiangxinwei@outlook.com>
2024-09-25 14:28:26 +0800

    revise the ntt benchmark

--- halo2_proofs/benches/fft.rs
+++ halo2_proofs/benches/fft.rs
@@ -7,18 +7,20 @@ use halo2_proofs::*;
 use halo2curves::pasta::Fp;
 
 use criterion::{BenchmarkId, Criterion};
-use rand_core::OsRng;
+use rand_core::{OsRng, SeedableRng};
+use rand_xorshift::XorShiftRng; // Faster than OsRng
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("fft");
-    for k in 3..19 {
+    for k in 3..=24 {
+        let omega = Fp::random(OsRng); // More realistic to use the same omega
         group.bench_function(BenchmarkId::new("k", k), |b| {
-            let mut a = (0..(1 << k)).map(|_| Fp::random(OsRng)).collect::<Vec<_>>();
-            let omega = Fp::random(OsRng); // would be weird if this mattered
+            let mut a = (0..(1 << k)).into_par_iter().map(|_| Fp::random(XorShiftRng::from_rng(OsRng).unwrap())).collect::<Vec<_>>(); // parallel data generation
             b.iter(|| {
                 best_fft(&mut a, omega, k as u32);
             });
-        });
+        }).sample_size(10);
     }
 }
 
