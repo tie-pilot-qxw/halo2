c53f1a2663f8aeaff7920cd37e1ff2bb28e1d20f
danzou1ge6 <yu_liukun@outlook.com>
2025-04-27 16:58:01 +0800

    修正编译错误

--- halo2_proofs/src/bin/debug_prover_lookup_cg_gen.rs
+++ halo2_proofs/src/bin/debug_prover_lookup_cg_gen.rs
@@ -183,14 +183,12 @@ fn main() {
             driver::ast2inst(cg_ret, allocator, &options, &hd_info, &driver::PanicJoinHandler::new()).unwrap();
         println!("[Test] End Compiling to Runtime Instructions");
 
-        let inputs = cg_inputs_shape.serialize(vec![vec![]], Tr::init(vec![]));
+        let mut inputs = cg_inputs_shape.serialize(vec![vec![]], Tr::init(vec![]));
 
         let mut runtime = driver::prepare_vm(
             rt_chunk,
             rt_const_tab,
             mem_allocator,
-            inputs,
-            zkpoly_runtime::runtime::ThreadPool::new(8),
             vec![zkpoly_cuda_api::mem::CudaAllocator::new(
                 0,
                 hd_info.gpu_memory_limit as usize,
@@ -199,7 +197,7 @@ fn main() {
         );
 
         println!("[Test] Launch VM");
-        let (r, _) = runtime.run(zkpoly_runtime::runtime::RuntimeDebug::None);
+        let (r, _) = runtime.run(&mut inputs, zkpoly_runtime::runtime::RuntimeDebug::None);
         println!("[Test] VM Exited");
 
         let proof = r.unwrap().unwrap_transcript_move().take().finalize();
